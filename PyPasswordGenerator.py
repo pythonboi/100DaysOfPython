f7b91c82a09f71a4de3afc5bac224e2e83f22500
pythonboi <hafeeztukur@gmail.com>
2021-08-04 07:39:13 -0400

    Created a passowrd generator

---
+++ PyPasswordGenerator.py
@@ -0,0 +1,85 @@
+# Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!\n")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+# Easy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+# I decide to group all the list in one big nested list since all the variable are list
+mulRandom = [letters, numbers, symbols]
+
+# sum all the integer together to have the total number and left of the character symbols to make
+allSum = nr_letters + nr_symbols + nr_numbers
+# print(allSum)
+# print(nr_letters)
+
+passwd = []
+
+# print(len(passwd))
+
+# Create a counter variable to be able to loop through the number of sequence needed
+
+count = 0
+synt = 0
+nmb = 0
+
+for char in letters:
+    # print(count)
+    if count < nr_letters:
+        count += 1
+        passwd.append(random.choice(letters))
+
+for sym in symbols:
+    if synt < nr_symbols:
+        synt += 1
+        passwd.append(random.choice(symbols))
+
+for num in numbers:
+    if nmb < nr_numbers:
+        nmb += 1
+        passwd.append(random.choice(numbers))
+
+# convert the list to a string using the join function
+
+lstStn = "".join(passwd)
+
+print(lstStn)
+
+
+
+
+
+# for name in letters:  # , symbols, numbers:
+#
+#     if count < allSum:
+#         count += 1
+#         lRandom = random.choice(letters)
+#         passwd.append(lRandom)
+# # print(count)
+#
+#
+# for chck in symbols:
+#     if len(passwd) < allSum:
+#         sRandom = random.choice(symbols)
+#         passwd.append(sRandom)
+# for n in numbers:
+#     if len(passwd) < allSum:
+#         nNum = random.choice(numbers)
+#         passwd.append(nNum)
+#
+# print(f'{lRandom}{sRandom}{nNum}')
+
+# print(passwd)
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
