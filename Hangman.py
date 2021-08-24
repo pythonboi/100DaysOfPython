87600cc03ca9c1133f95b75cb58c7b73190bfc6d
pythonboi <hafeeztukur@gmail.com>
2021-08-24 01:42:26 -0400

    modify the code update the for loop for display

--- Hangman.py
+++ Hangman.py
@@ -14,31 +14,27 @@ display = []
 # Printing out the guess word
 print(f"Psst, the guess word is:", chosen_word)
 
-# Print out the Guess Letter
-print("Guess a Letter: ", end="")
-
-# Take input from the user from the Keyboard
-guess = input()
-
-# change the input character from Capital letter to Lowercase letter of the use input
-guess = guess.lower()
-
-count = 0
-
+# For loop for creating the dash sign
 for char in chosen_word:
     # display += "_"
     display.append("_")
 # print(display)
 
-# for num in range(len(chosen_word)):
-#     # for check in chosen_word:
-#     count = count + 1
-#     print(count)
+while True:
 
-# Making a For loop to count the number of guess and make an index of that number with the missing blanks
+    # Print out the Guess Letter
+    print("Guess a Letter: ", end="")
 
-for check in range(len(chosen_word)):
-    if guess in chosen_word[check]:
-        display[check] = guess
+    # Take input from the user from the Keyboard
+    guess = input()
 
-print(display)
+    # change the input character from Capital letter to Lowercase letter of the use input
+    guess = guess.lower()
+
+    # Making a For loop to count the number of guess and make an index of that number with the missing blanks
+
+    for check in range(len(chosen_word)):
+        if guess in chosen_word[check]:
+            display[check] = guess
+
+    print(display)
