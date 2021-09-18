d313b8ce3e03f08ab6813f33bd8ec7e54e897a7b
pythonboi <hafeeztukur@gmail.com>
2021-09-18 16:23:30 -0400

    added art.py for logo and fix the shift buy for number more than alphabet characters

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,5 +1,9 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
+
+print(art.logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -10,8 +14,10 @@ def caesar(text, shift, direction):
     codeChar = ''
     if direction == 'encode':
         for me in text:
+            if shift > len(alphabet):
+                shift = shift % len(alphabet)
             ch = alphabet.index(me) + shift
-            if ch > len(alphabet):
+            if ch >= len(alphabet):
                 alpLeft = ch - len(alphabet)
                 codeChar += alphabet[alpLeft]
             else:
@@ -21,6 +27,9 @@ def caesar(text, shift, direction):
     elif direction == 'decode':
         decChar = ''
         for new in text:
+            if shift > len(alphabet):
+                shift = shift % len(alphabet)
+                # After code modification
             deco = alphabet.index(new) - shift
             decChar += alphabet[deco]
         print(f'The decoded text is {decChar}')
