caeccc091850300000ea31ca1f150ef65b97efe5
pythonboi <hafeeztukur@gmail.com>
2021-09-25 19:05:04 -0400

    improve the while loop and code functionality

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,65 +1,77 @@
+# Import the art ASCII logo
 import art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
+# Print the art logo from the import art
 print(art.logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+
+# create a function for 3 parameters text, shift, direction
+
+def caesar(text, shift, direction):
+
+    # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    # text = input("Type your message:\n").lower()
+    # shift = int(input("Type the shift number:\n"))
+
+    codeChar = ''
+    # check and validate direction
+    if direction == 'encode':
+        # loop through the text
+        for me in text:
+            if shift > len(alphabet):
+                shift = shift % len(alphabet)
+# check variable or value not in the alphabet list
+            if me not in alphabet:
+                codeChar += me
+            else:
+                ch = alphabet.index(me) + shift
+# Check if the ch variable is greater than the total alphabet character in the list
+            if ch > len(alphabet):
+                alpLeft = ch - len(alphabet)
+                codeChar += alphabet[alpLeft]
+            else:
+                codeChar += alphabet[ch]
+
+        print(f'The encoded text is {codeChar}')
+
+# check if the direction string match decode
+    elif direction == 'decode':
+        decChar = ''
+        for new in text:
+            if shift > len(alphabet):
+                shift = shift % len(alphabet)
+                # After code modification
+            if new not in alphabet:
+                decChar += new
+            else:
+                deco = alphabet.index(new) - shift
+                decChar += alphabet[deco]
+
+        print(f'The decoded text is {decChar}')
+
+
+caesar(text=text, shift=shift, direction=direction)
+
 while True:
 
     again = input("Type 'yes' if you want to go again. Otherwise type 'no'. ""\n").lower()
 
     if again == 'yes':
-
         direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
         text = input("Type your message:\n").lower()
         shift = int(input("Type the shift number:\n"))
 
-        # create a function for 3 parameters text, shift, direction
-
-        def caesar(text, shift, direction):
-            codeChar = ''
-            # check and validate direction
-            if direction == 'encode':
-                # loop through the text
-                for me in text:
-                    if me not in alphabet:
-                        codeChar += me
-                    else:
-                        ch = alphabet.index(me) + shift
-                    # check shift value greater than the len of alphabet
-
-                    if shift > len(alphabet):
-                        shift = shift % len(alphabet)
-
-                    if ch > len(alphabet):
-                        alpLeft = ch - len(alphabet)
-                        codeChar += alphabet[alpLeft]
-                    else:
-                        codeChar += alphabet[ch]
-
-                print(f'The encoded text is {codeChar}')
-
-            elif direction == 'decode':
-                decChar = ''
-                for new in text:
-                    if shift > len(alphabet):
-                        shift = shift % len(alphabet)
-                        # After code modification
-                    if new not in alphabet:
-                        decChar += new
-                    else:
-                        deco = alphabet.index(new) - shift
-                        decChar += alphabet[deco]
-                print(f'The decoded text is {decChar}')
+        caesar(text=text, shift=shift, direction=direction)
     elif again == "no":
         exit()
     else:
         print("Please answer 'yes' or 'no'")
 
-
-caesar(text=text, shift=shift, direction=direction)
+# caesar(text=text, shift=shift, direction=direction)
