eb6730fda65cc3064c0b552df00d774bf13614b3
pythonboi <hafeeztukur@gmail.com>
2021-10-03 14:42:19 -0400

    update the code and import replit/n added while loop and if statement \n

--- SecretAuction.py
+++ SecretAuction.py
@@ -1,4 +1,7 @@
+# import replit
+
 from art import logo2
+from replit import clear
 
 print(logo2)
 
@@ -6,9 +9,32 @@ print("Welcome to the Secret Auction program")
 
 auction = {}
 
-name = input("What is your name?: ").title()
-bid = int(input("What is your bid?: "))
+print(type(auction))
 
-question = input("Are there any other bidders? Type 'yes or 'no.' ")
+collect = ''
+
+name = input("What is your name?: ").title()
+bid = int(input("What is your bid?: $"))
+
+while True:
+
+    question = input("Are there any other bidders? Type 'yes' or 'no.' ").lower()
+
+    auction[name] = name
+    auction[name] = bid
+
+    if question == "yes":
+        clear()
+        # replit.clear()
+    elif question == "no":
+        print("GoodBye")
+        exit()
+
+print(type(auction))
+
+# auction.update(name=bid)
 
 print(auction)
+print(collect)
+
+# print(f"The winner is {name} with a bid of {bid}.")
