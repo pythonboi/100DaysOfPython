315dae4d8636f26cf99acfebd7b185ac0762506d
pythonboi <hafeeztukur@gmail.com>
2021-10-03 23:49:00 -0400

    added while loop and for loop

--- SecretAuction.py
+++ SecretAuction.py
@@ -1,4 +1,5 @@
-# import replit
+import replit
+import os
 
 from art import logo2
 from replit import clear
@@ -9,32 +10,41 @@ print("Welcome to the Secret Auction program")
 
 auction = {}
 
-print(type(auction))
-
-collect = ''
-
 name = input("What is your name?: ").title()
 bid = int(input("What is your bid?: $"))
+
+auction[name] = name
+auction[name] = bid
 
 while True:
 
     question = input("Are there any other bidders? Type 'yes' or 'no.' ").lower()
 
-    auction[name] = name
-    auction[name] = bid
-
     if question == "yes":
-        clear()
-        # replit.clear()
+        replit.clear()
+        # os.system('cls')
+        # clear()
+        name = input("What is your name?: ").title()
+        bid = int(input("What is your bid?: $"))
+        auction[name] = name
+        auction[name] = bid
+
     elif question == "no":
         print("GoodBye")
-        exit()
-
-print(type(auction))
-
-# auction.update(name=bid)
+        break
+    else:
+        print("Answer 'yes' or 'no'")
 
 print(auction)
-print(collect)
 
-# print(f"The winner is {name} with a bid of {bid}.")
+getHigh = ''
+
+for check, val in auction.items():
+    if val >= val:
+        highVal = val
+    print(highVal)
+# highBid = auction.get(max(auction))
+
+# print(f"The winner is  with a bid of {highBid}.")
+
+# print(type(auction))
