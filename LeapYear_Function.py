1f368ce8fb60e638faa7dbf636106779b426fb6a
pythonboi <hafeeztukur@gmail.com>
2021-10-09 23:05:59 -0400

    modify the print to return and if statement

--- LeapYear_Function.py
+++ LeapYear_Function.py
@@ -2,17 +2,27 @@ def is_leap(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                print("Leap year.")
+                return True
+                # print("Leap year.")
             else:
-                print("Not leap year.")
+                return False
+                # print("Not leap year.")
         else:
-            print("Leap year.")
+            return True
+            # print("Leap year.")
     else:
-        print("Not leap year.")
+        return False
+        # print("Not leap year.")
 
 
-def days_in_month():
+def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    print(is_leap(year))
+    if is_leap(year):
+        for months in month_days:
+
+
 
 
 # 🚨 Do NOT change any of the code below
