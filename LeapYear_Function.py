17373be6bf8019775f82156fbcc3267236774764
pythonboi <hafeeztukur@gmail.com>
2021-10-15 02:22:33 -0400

    modified the code to be more efficient and added if to check if year and month are leap year

--- LeapYear_Function.py
+++ LeapYear_Function.py
@@ -18,31 +18,35 @@ def is_leap(year):
 def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    if not is_leap(year):
-        # for months in range(1, len(month_days)):
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
 
-        if month == 1:
-            return month_days[0]
-        elif month == 2:
-            return month_days[1]
-        elif month == 3:
-            return month_days[2]
-        elif month == 4:
-            return month_days[3]
-        elif month == 5:
-            return month_days[6]
-        elif month == 6:
-            return month_days[7]
-        elif month == 7:
-            return month_days[8]
-        elif month == 8:
-            return month_days[9]
-        elif month == 9:
-            return month_days[10]
-        elif month == 10:
-            return month_days[11]
-        elif month == 11:
-            return month_days[12]
+    # if not is_leap(year):
+    #     # for months in range(1, len(month_days)):
+    #
+    #     if month == 1:
+    #         return month_days[0]
+    #     elif month == 2:
+    #         return month_days[1]
+    #     elif month == 3:
+    #         return month_days[2]
+    #     elif month == 4:
+    #         return month_days[3]
+    #     elif month == 5:
+    #         return month_days[6]
+    #     elif month == 6:
+    #         return month_days[7]
+    #     elif month == 7:
+    #         return month_days[8]
+    #     elif month == 8:
+    #         return month_days[9]
+    #     elif month == 9:
+    #         return month_days[10]
+    #     elif month == 10:
+    #         return month_days[11]
+    #     elif month == 11:
+    #         return month_days[12]
 
 
 # 🚨 Do NOT change any of the code below
