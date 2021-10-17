46ed95be9648d9ebb48d3211c693cfb5fb7101cf
pythonboi <hafeeztukur@gmail.com>
2021-10-17 19:43:56 -0400

    added a dictionary, for loop and iteration

--- Calculator.py
+++ Calculator.py
@@ -20,3 +20,31 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+# operations["+"] = ["+"]
+# operations["+"] = add(n1=5, n2=5)
+
+num1 = int(input("What is the first number?: "))
+num2 = int(input("What is the second number? "))
+
+for operation in operations:
+    print(operation)
+
+signs = input("Pick an operation from the line above: ")
+# result = ''
+
+for symbol in operations:
+    if signs == symbol:
+        result = operations[symbol]
+        finalResult = result(num1, num2)
+
+
+print(f"{num1} {signs} {num2} = {finalResult}")
