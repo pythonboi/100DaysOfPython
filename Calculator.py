5df0040eff73654018f818066d4b8fe562c00182
pythonboi <hafeeztukur@gmail.com>
2021-10-16 23:49:49 -0400

    create a function

---
+++ Calculator.py
@@ -0,0 +1,22 @@
+# Create a function for Adding
+
+def add(n1, n2):
+    return n1 + n2
+
+
+# Create a function for Subtracting
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+# Create a function for Multiplication
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+# Create a function for Division
+
+def divide(n1, n2):
+    return n1 / n2
