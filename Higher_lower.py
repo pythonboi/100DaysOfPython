841105231cc03d8ef10483e5fa3e3e082290e3dd
pythonboi <hafeeztukur@gmail.com>
2021-12-11 02:33:31 -0500

    Don't know what i am doing again

--- Higher_lower.py
+++ Higher_lower.py
@@ -56,6 +56,7 @@ b = data[selectB].get('follower_count')
 
 print(a)
 print(b)
+print("before the while loop")
 
 while True:
 
@@ -64,12 +65,17 @@ while True:
 
     choose = input(f"Who has more followers? Type 'A' or 'B': ").lower()
 
-    def getSocialName():
-        a = data[selectA].get('follower_count')
-        b = data[selectB].get('follower_count')
-        if counter > 1:
-            newA = b
+    # def getSocialName():
+    #     a = data[selectA].get('follower_count')
+    #     b = data[selectB].get('follower_count')
+    #     if counter > 1:
+    #         newA = b
 
+    # Code below checking and evaluating the code process
+
+    print("inside the evaluate function")
+
+    # counter = 0
 
     if choose == 'a':
         if a > b:
@@ -86,44 +92,66 @@ while True:
     if choose == 'a':
         if a < b:
             print(f"Sorry, that's wrong. Final score: {counter}")
+            # exit()
             break
 
     elif choose == 'b':
         if b < a:
             print(f"Sorry, that's wrong. Final score: {counter}")
+            # exit()
             break
 
-    # if optionA > optionB:
-    #     counter = counter + 1
-    #     print(f"You're right! Current score: {counter}.")
+        # if optionA > optionB:
+        #     counter = counter + 1
+        #     print(f"You're right! Current score: {counter}.")
 
-    # elif optionB > optionA:
-    #     counter = counter + 1
-    #     print(f"You're right! Current score: {counter}")
+        # elif optionB > optionA:
+        #     counter = counter + 1
+        #     print(f"You're right! Current score: {counter}")
 
     else:
         print(f"Sorry, that's wrong. Final score: {counter}")
         break
 
-    if True:
+    # evaluate()
 
-        # counter != 0:
+    def getA():
+        needB = data[selectB].get('follower_count')
+        if counter > 1:
+            needA = needB
+            print(needA)
+            a = needA
+
+
+    if True:
         switch = f"{option1} {getB}"
         print(switch)
-        getSocialName()
+
+        # thisB = b
+
+        print(type(switch))
+        # if counter > 2:
+        #     switch = f"{option1} {getC()}"
+        getA()
 
     print(vs)
 
-    selectC = random.randint(0, pick)
 
-    infoNameC = data[selectC].get('name')  #
-    infoDescrC = data[selectC].get('description')
-    infoCountryC = data[selectC].get('country')
-    c = data[selectC].get('follower_count')
-    # print(c)
-    b = c
+    def getC():
+
+        selectC = random.randint(0, pick)
+
+        infoNameC = data[selectC].get('name')  #
+        infoDescrC = data[selectC].get('description')
+        infoCountryC = data[selectC].get('country')
+        c = data[selectC].get('follower_count')
+        # print(c)
+        b = c
+        print(b)
+
+        print(f"{option2} {infoNameC}, {aLetter} {infoDescrC}, {frm} {infoCountryC}.")
+
+
+    getC()
+    print(a)
     print(b)
-
-    print(f"{option2} {infoNameC}, {aLetter} {infoDescrC}, {frm} {infoCountryC}.")
-
-
