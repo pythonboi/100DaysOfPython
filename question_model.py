d98aec2c815bc3897aa91e44ca29867b21198ba2
pythonboi <hafeeztukur@gmail.com>
2022-01-05 18:27:59 -0500

    adding new directory

---
+++ question_model.py
@@ -0,0 +1,9 @@
+class Question:
+    """Creating/Defining a Question class to be use as blueprint """
+
+    def __init__(self, text, answer):
+        """Initializing the attributes"""
+        self.text = text
+        self.answer = answer
+
+
