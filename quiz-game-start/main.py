478f491a8ffb0c53d0fe8e7c658a9323d07ed2a3
pythonboi <hafeeztukur@gmail.com>
2022-01-13 23:56:16 -0500

    made changes to main.py and fix the question_bank

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 
 question_bank = []
@@ -7,12 +8,18 @@ question_bank = []
 
 for question in question_data:
     myQue = Question(text=question['text'], answer=question['answer'])
-    question_bank.append(myQue.text)
-    question_bank.append(myQue.answer)
+    #newQue
+    question_bank.append(myQue)
+    #question_bank.append(myQue.answer)
+
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 
 print(question_bank)
 
 print(myQue.text)
+print(myQue.answer)
 
 # lenQue = (question_bank[1])
 # print(lenQue)
