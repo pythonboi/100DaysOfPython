5058a4186ecd207b9ebe318df67c77ed529a667c
pythonboi <hafeeztukur@gmail.com>
2022-03-09 23:48:01 -0500

    Create two function one 4 multicolor and second function for drawing circle

--- Spirograph.py
+++ Spirograph.py
@@ -1,2 +1,24 @@
 import random
+import turtle as t
 
+t.colormode(255)
+
+
+def multiColor():
+    r = random.random()
+    g = random.random()
+    b = random.random()
+
+    t.pencolor(r,g,b)
+
+def drawCircle(x):
+
+    
+    t.circle(100)
+    t.tilt(10)
+    t.fd(20)
+    t.circle(95)
+
+drawCircle(multiColor)
+
+t.exitonclick()
