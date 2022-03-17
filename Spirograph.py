342089424828477f6bc7f993d20d6e66158510b8
pythonboi <hafeeztukur@gmail.com>
2022-03-17 19:11:08 -0400

    update the comment on the code

--- Spirograph.py
+++ Spirograph.py
@@ -1,15 +1,21 @@
+# Import both the Random and Turtle module 
 import random
 import turtle as t
 
+# Instantiate the Turtle class 
 tp = t.Turtle()
 
+# make the speed of the circle drawing very fast 
 tp.speed("fastest")
+
+# Create a window screen size of widht and height 
 t.setup(850, 750)
 
+# Assign a radius variable of 100
 r = 100
 
 
-
+# Create a function to make randoms colors 
 def multiColor():
     r = random.random()
     g = random.random()
@@ -18,7 +24,7 @@ def multiColor():
     tp.color(r,g,b)
  
 
-
+# for loop to 
 for _ in range(73):
 
     tp.pensize(1)
@@ -27,4 +33,5 @@ for _ in range(73):
     multiColor()
 
 
+# exit the window once the task is completed upon click on the window screen
 t.exitonclick()
