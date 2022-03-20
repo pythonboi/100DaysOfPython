172e2493f9aff8d0fee4e2712be47af7bf8a2e9f
pythonboi <hafeeztukur@gmail.com>
2022-03-19 23:55:34 -0400

    add a function to calculate the size of the circle

--- Spirograph.py
+++ Spirograph.py
@@ -23,15 +23,19 @@ def multiColor():
 
     tp.color(r,g,b)
  
+# Create a function with argument
+def draw_spirograph(size_of_gap):
 
-# for loop to 
-for _ in range(73):
+# for loop to divide the total size of circle and divide by the argument
+    for _ in range(int(360 / size_of_gap)):
 
-    tp.pensize(1)
-    tp.circle(r * 1)
-    tp.left(5)
-    multiColor()
+        tp.pensize(1)
+        tp.circle(r)
+        tp.left(size_of_gap)
+        multiColor()
 
+# Call the function 
+draw_spirograph(5)
 
 # exit the window once the task is completed upon click on the window screen
 t.exitonclick()
