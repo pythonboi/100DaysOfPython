16849b7b5fc8fd7b387504f9248a63679ed82970
pythonboi <hafeeztukur@gmail.com>
2022-03-28 02:56:04 -0400

    comment out the first code

--- hirst_Painting.py
+++ hirst_Painting.py
@@ -1,22 +1,19 @@
 # Import the Cologram module 
 import colorgram
 
-getImages = colorgram.extract(r"C:\Users\admin\Documents\PyDev\100DaysOfPython\hirst.jpg", 10)
+getImages = colorgram.extract(r"C:\Users\admin\Documents\PyDev\100DaysOfPython\hirst.jpg", 20)
 
 myDic = []
 
-#print(type(getImages))
+# print(type(getImages))
 # print(getImages)
 
-
-rpg = getImages[9]
-
-#print(rpg.rgb)
 
 for m in getImages:
     # myDic.append(m.rgb)
     red, green, blue = m.rgb.r, m.rgb.g, m.rgb.b
     myTuple = (red, green, blue)
     myDic.append(myTuple)
-    #print(red, green, blue)
+    # print(red, green, blue)
 print(myDic)
+
