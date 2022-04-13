2646725cc281673bacbdba20873a5ab77a778bfa
pythonboi <hafeeztukur@gmail.com>
2022-04-13 08:07:29 -0400

    new code to make snake game

---
+++ Snake_game/main.py
@@ -0,0 +1,51 @@
+import time
+from turtle import Turtle, Screen
+import random
+
+myScreen = Screen()
+
+snakePosition = [0, -20, -40]
+snake_draw = []
+# snake_color = ['white', 'white', 'white']
+
+is_running = True
+
+myScreen.setup(width=600, height=600)
+myScreen.bgcolor("black")
+myScreen.title("Snake_Game ")
+
+myScreen.tracer(0)
+
+# myT.shape("square")
+# myT.color('white')
+# myT.penup()
+# myT.goto(0, 0)
+# snake_draw.append(myT)
+#
+# myT.shape("square")
+# myT.color('white')
+# myT.penup()
+# myT.goto(-20, 20)
+# snake_draw.append(myT)
+
+
+for build in range(0, 3):
+    myT = Turtle()
+    myT.shape("square")
+    myT.color('white')
+    myT.penup()
+    myT.goto(snakePosition[build], 0)
+    snake_draw.append(myT)
+
+
+# if snakePosition == 3:
+#     is_running = True
+
+
+while is_running:
+    myScreen.update()
+    time.sleep(1)
+    for snake in snake_draw:
+        snake.forward(10)
+
+myScreen.exitonclick()
