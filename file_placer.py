f6db5f1c7511718cd3bf7518b17e5f7e2306aa85
pythonboi <hafeeztukur@gmail.com>
2022-05-28 18:22:49 -0400

    fix the bug for shutil file error for file already exited in the directory path

--- file_placer.py
+++ file_placer.py
@@ -16,11 +16,6 @@ fullPath = os.path.join(chgeDirectory, getfileList)
 getFilename = []
 myAppend = []
 
-dest = os.path.join(chgeDirectory, )
-dest1 = os.path.abspath(chgeDirectory)
-print(dest, dest1)
-
-print(type(newSet))
 
 if currentDirectory != chgeDirectory:
     os.chdir(chgeDirectory)
@@ -32,16 +27,11 @@ if currentDirectory != chgeDirectory:
         newFolder = input("Please Enter the name of the new Folder: ").title()
 
         if os.path.exists(chgeDirectory):
-            # for v in chgeDirectory:
-            #     print(v)
 
             lstDir = os.listdir(os.getcwd())
             for v in lstDir:
                 getfileList += v
 
-            # print(getfileList)
-            # print(os.path.join(chgeDirectory, getfileList))
-            # print(lstDir)
             print(len(lstDir), f"items in {chgeDirectory}")
 
         for check in lstDir:
@@ -58,11 +48,6 @@ if currentDirectory != chgeDirectory:
         # print(len(fileSide))
         print(fileSide)
 
-        # Here is printing all the file in the directory
-        # print(fileSide)
-        # for v in folderSide:
-        #     fullPath += os.path.join(chgeDirectory, v)
-
         if newFolder not in folderSide:
             os.mkdir(newFolder)
 
@@ -71,13 +56,8 @@ if currentDirectory != chgeDirectory:
             print(f"{newFolder} folder already exits")
             # exit()
 
-        # if os.path.exists(fullPath):
-        #     print(fullPath)
-        #     print("Yes")
 
-        # Differentiate the file from the extension
-
-        # Commenting this for issue that needs to be fix
+# Differentiate the file from the extension
 
         for f in fileSide:
 
@@ -89,7 +69,7 @@ if currentDirectory != chgeDirectory:
 
                 # print(splitFileExt)
 
-                # Using regular Expression to separate the file extension from the file name
+# Using regular Expression to separate the file extension from the file name
 
                 for ext in fileExt:
                     regExt = re.search(r'\.(\w+)', fileExt)
@@ -97,9 +77,7 @@ if currentDirectory != chgeDirectory:
 
         # print(newfileExt)
 
-        # Need to fix the issue above
-
-        # Making the extension to be unique in the file
+# Making the extension to be unique in the file
 
         for uni in newfileExt:
             newSet.add(uni.lower())
@@ -109,7 +87,7 @@ if currentDirectory != chgeDirectory:
         print("Here is the second set side")
         print(len(newSet))
 
-        # Creating the Folder from the file extension
+# Creating the Folder from the file extension
 
         for uniqFile in newSet:
             try:
@@ -126,104 +104,20 @@ if currentDirectory != chgeDirectory:
 
         if os.path.isfile(vryFile):
 
-            # getfile, getnewFileExt = os.path.splitext(vryFile)
-            # getExtFilePath = os.path.join(chgeDirectory, getfile)
-            # print(vryFile)
             fullPath = os.path.join(chgeDirectory, vryFile)
-            print(fullPath)
-            #print(len(fullPath))
-            fileName, fileExt = os.path.splitext(vryFile)
-
-            # print(getExtFilePath)
 
         for chkFolder in folderSide:
-                # print(chkFolder)
+            # print(chkFolder)
             getExistingDir = os.path.join(chgeDirectory, chkFolder)
 
-            for word in newSet:
+            try:
+
+                for word in newSet:
                 # print(word)
-                if word in fullPath and word in getExistingDir:
-
-                    print(shutil.copy2(fullPath, getExistingDir))
-
-
-            #print(getExistingDir)
-
-                # fileType = input("Please Enter the file format you want to copy: ").lower()
-                #
-                # if fullPath.endswith(fileType) and fileType == os.path.isdir(getExistingDir):
-                #     shutil.copy2(fullPath, getExistingDir) # and fullPath.endswith(fileExt) == os.path.isdir(chkFolder):
-
-                    # print(fileExt)
-                    # shutil.copy(fullPath, getExistingDir)
-
-                # if getExistingDir.endswith(getExtFilePath):
-                #     shutil.copy(vryFile, getExistingDir)
-                # print(vryFile)
-                # pr and chkFolder in getnewFileExt:
-
-                # os.chdir(os.path.join(chgeDirectory, chkFolder))
-                # shutil.copy(getfile, getnewFileExt)
-
-                # print(getExistingDir)
-
-    # for vryFile in lstDir:
-    #
-    #     if os.path.isfile(vryFile):
-    #
-    #         getfile, getnewFileExt = os.path.splitext(vryFile)
-    #         getExtFilePath = os.path.join(chgeDirectory, getfile)
-    #         print(getExtFilePath)
-    #
-    #         for chkFolder in folderSide:
-    #             # print(chkFolder)
-    #             getExistingDir = os.path.join(chgeDirectory, chkFolder)
-    #             # print(getExistingDir)
-    #
-    #             if getExistingDir.endswith(getExtFilePath):
-    #                 shutil.copy(vryFile, getExistingDir)
-    #                 # print(vryFile)
-    #                 # pr and chkFolder in getnewFileExt:
-    #
-    #                 # os.chdir(os.path.join(chgeDirectory, chkFolder))
-    #                 # shutil.copy(getfile, getnewFileExt)
-    #
-    #             # print(getExistingDir)
-
-    #     if newGetSet in chkFolder:
-    #         shutil.copyfile(getfile, getExistingDir)
-    #         # myAppend.append("yes")
-    #         # print("yes")
-    #
-    #     else:
-    #         print("no")
-    #
-    # print(len(myAppend))
-
-    # if newGetSet == chkFolder:
-    #     print("yes")
-    # else:
-    #     print("No")
-
-    # print(os.path.isdir(vryFile))
-    # shutil.copyfile(getfile, vryFile)
-
-    # print(getnewFileExt)
-    # else:
-    #     print("false")
-
-    # for chkFolder in folderSide:
-    #     getExistingDir = os.path.join(chgeDirectory, chkFolder)
-    #     print(getExistingDir)
-
-# for vryFile in fileSide:
-#
-#     if vryFile.endswith()
-#     if vryFile.endswith('rdp'):
-#         print(f"{vryFile}")
-#         # shutil.copyfile()
-#
-#     pass
+                    if word in fullPath and word in getExistingDir:
+                        print(shutil.move(fullPath, getExistingDir))
+            except shutil.Error:
+                print("file already exits")
 
 # urlLink = "https://www.youtube.com/watch?v=n3IYVthup9I"
 #
